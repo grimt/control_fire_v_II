364c0186c12c28d5b4d120821fbeb64292c12796
grimt <graeme.john.thomson@gmail.com>
2016-03-02 20:58:59 +1300

    Write desired temperature to file

--- remote_temp_server.py
+++ remote_temp_server.py
@@ -27,10 +27,19 @@ while 1:
     temp_str = conn.recv(1024)
     data = temp_str.split(':')
     if data[0] = 'R':
-        print 'Remote Reaquired temperature: ' + data[1]
+        print 'Remote Required temperature: ' + data[1]
+        #update the required temp file
+        try:
+            f = open ('/tmp/temperature.txt','wt')
+            f.write (data[1])
+            f.close ()
+        except IOError:
+            if my_fire.debug_level >=2:
+                print ("Cant open file temperature.txt for reading")
+            my_logger.exception("Cant open file temperature.txt for reading")
     else if data[0]= 'M':
-
         print 'Remote Measured temperature: ' + data[1]
+        #update the measured temperature file
 
     # Next write temperature to a file.
     if not data: 
