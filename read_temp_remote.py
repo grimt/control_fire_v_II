ed33066fcfd632ba212ba170bd097547bcfd1356
grimt <graeme.john.thomson@gmail.com>
2016-04-23 06:12:30 +0000

    Add code to read buttons

--- read_temp_remote.py
+++ read_temp_remote.py
@@ -38,21 +38,50 @@ def write_desired_temperature_to_file(temperature):
             print ("Cant open file temperature.txt for writing")
 
 
+def pin_12_callback(channel):
+        # White Button
+        print ('Pressed button 12 White')
+        desired_temperature = read_desired_temperature_from_file()
+        desired_temperature = 19 
+        write_desired_temperature_to_file(desired_temperature)
+        #Send temperature to remote pi is done in the while loop below
+
+
+
 def pin_18_callback(channel):
-        print ('Pressed button 18')
-	desired_temperature = read_desired_temperature_from_file()
-	desired_temperature = desired_temperature + 1
-	write_desired_temperature_to_file(desired_temperature)
-	#Send temperature to remote pi
+        # Green Button
+        print ('Pressed button 18 Green')
+        desired_temperature = read_desired_temperature_from_file()
+        desired_temperature = 999 
+        write_desired_temperature_to_file(desired_temperature)
+        #Send temperature to remote pi is done in the while loop below
 
 def pin_23_callback(channel):
-        print ('pressed button 23')
-	desired_temperature = read_desired_temperature_from_file()
-	desired_temperature = desired_temperature - 1
-	write_desired_temperature_to_file(desired_temperature)
-        # send temperature to remote pi
-        
-def write_measured_temp_to_led (temp):
+        # Blue Button
+        print ('pressed button 23 Blue')
+        desired_temperature = read_desired_temperature_from_file()
+        if desired_temperature > 0:
+          desired_temperature = desired_temperature - 1
+          write_desired_temperature_to_file(desired_temperature)
+        # send temperature to remote pi is done in the while loop below.
+
+def pin_24_callback(channel):
+        # Yellow button
+        print ('pressed button 24 Yellow')
+        desired_temperature = read_desired_temperature_from_file()
+        desired_temperature = desired_temperature + 1
+        write_desired_temperature_to_file(desired_temperature)
+        # send temperature to remote pi is done in the while loop below.
+
+def pin_25_callback(channel):
+        # Red button
+        print ('pressed button 25 Red')
+        desired_temperature = read_desired_temperature_from_file()
+        desired_temperature = 0
+        write_desired_temperature_to_file(desired_temperature)
+        # send temperature to remote pi is done in the while loop below.       
+
+def write_temp_to_led (temp):
   segment.writeDigit(0, int(temp / 10))     # Tens
   segment.writeDigit(1, int(temp % 10))          # Ones
   decimal = temp - int(temp)
@@ -62,12 +91,25 @@ def write_measured_temp_to_led (temp):
   # Toggle colon
   segment.setColon(1)              # Toggle colon at 1Hz 
 
+GPIO.setup (12, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.add_event_detect (12, GPIO.FALLING, callback=pin_12_callback, bouncetime=1000)
+
+
+
 GPIO.setup (18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.add_event_detect (18, GPIO.FALLING, callback=pin_18_callback, bouncetime=1000)
 
 
 GPIO.setup (23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.add_event_detect (23, GPIO.FALLING, callback=pin_23_callback, bouncetime=1000)
+
+GPIO.setup (24, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.add_event_detect (24, GPIO.FALLING, callback=pin_24_callback, bouncetime=1000)
+
+GPIO.setup (25, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.add_event_detect (25, GPIO.FALLING, callback=pin_25_callback, bouncetime=1000)
+
+
 
 
 port = 5000;
@@ -114,7 +156,7 @@ while True:
         except socket.error:
             #Send failed
             print 'Send failed'
-        write_measured_temp_to_led(temperature)
+        write_temp_to_led(temperature)
     else:
         print 'Failed to get reading. Try again!'
 
