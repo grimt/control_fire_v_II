d7b68565e58e2c3fa6eedff199725cf4319fde44
grimt <graeme.john.thomson@gmail.com>
2016-05-11 06:27:37 +0000

    Fix some bugs and add while loop

--- remote_read_desired_temperature_buttons.py
+++ remote_read_desired_temperature_buttons.py
@@ -38,30 +38,28 @@ def pin_18_callback(channel):
         print ('Pressed button 18 Green')
         desired_temperature = 999 
         write_desired_temperature_to_file(desired_temperature)
-        #Send temperature to remote pi is done in the while loop below
 
 def pin_23_callback(channel):
         # Blue Button
         print ('pressed button 23 Blue')
-        desired_temperature = read_desired_temperature_from_file()
+        desired_temperature = int(read_desired_temperature_from_file())
         if desired_temperature > 0:
           desired_temperature = desired_temperature - 1
-          write_desired_temperature_to_file(desired_temperature)
-        # send temperature to remote pi is done in the while loop below.
+          write_desired_temperature_to_file(str(desired_temperature))
 
 def pin_24_callback(channel):
         # Yellow button
         print ('pressed button 24 Yellow')
-        desired_temperature = read_desired_temperature_from_file()
+        desired_temperature = int(read_desired_temperature_from_file())
         desired_temperature = desired_temperature + 1
-        write_desired_temperature_to_file(desired_temperature)
+        write_desired_temperature_to_file(str(desired_temperature))
         # send temperature to remote pi is done in the while loop below.
 
 def pin_25_callback(channel):
         # Red button
         print ('pressed button 25 Red')
         desired_temperature = 0
-        write_desired_temperature_to_file(desired_temperature)
+        write_desired_temperature_to_file(str(desired_temperature))
         # send temperature to remote pi is done in the while loop below.
 
 
@@ -86,3 +84,7 @@ GPIO.add_event_detect (24, GPIO.FALLING, callback=pin_24_callback, bouncetime=10
 GPIO.setup (25, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.add_event_detect (25, GPIO.FALLING, callback=pin_25_callback, bouncetime=1000)
 
+
+while True:
+    #don't do much
+    balls_variable = 1
