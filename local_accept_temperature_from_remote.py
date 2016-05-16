71e62bc40518c5ae62dd7a7d550c53490ae035e0
grimt <graeme.john.thomson@gmail.com>
2016-05-16 20:48:16 +1200

    Remove all debug prints
    Some will be re-added a logs

--- local_accept_temperature_from_remote.py
+++ local_accept_temperature_from_remote.py
@@ -11,30 +11,30 @@ HOST = '192.168.1.151'   # Symbolic name meaning all available interfaces
 PORT = 5000 # Arbitrary non-privileged port
   
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-print 'Socket created'
+# print 'Socket created'
    
 try:
     s.bind((HOST, PORT))
 except socket.error , msg:
-    print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+    # print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
     sys.exit()
                     
-print 'Socket bind complete'
+# print 'Socket bind complete'
             
 s.listen(10)
-print 'Socket now listening'
+# print 'Socket now listening'
                       
 #now keep talking with the client
 while 1:
 #wait to accept a connection - blocking call
     conn, addr = s.accept()
-    print 'Connected with ' + addr[0] + ':' + str(addr[1])
+    # print 'Connected with ' + addr[0] + ':' + str(addr[1])
                                            
     temp_str = conn.recv(1024)
     data = temp_str.split(':')
     
     if data[0] is 'R':
-        print 'Remote Required temperature: ' + data[1]
+        # print 'Remote Required temperature: ' + data[1]
         #update the required temp file
         if int (data[1]) != 555:
             try:
